a1541e32b36f80980ab63ad69aabd2933cf7da75
LUCAS FERNANDES <060457@TCB.local>
2022-08-23 17:28:16 -0300

    Ta quase acabando

--- livro/forms.py
+++ livro/forms.py
@@ -1,8 +1,23 @@
 from django import forms
-from .models import Livros
+from .models import Livros, Categoria
+from django.db.models import fields
+from django.db import models
+from datetime import date
 
 
 class Cadastro_livro(forms.ModelForm):
     class Meta:
         model = Livros
-        fields = ('nome', 'autor', 'co_autor', 'data_cadastro', 'emprestado', 'categoria', 'usuario')
+        fields = ('__all__')
+
+    def __int__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['usuario'].widget = forms.HiddenInput()
+
+class Cadastro_categoria(forms.Form):
+    nome = forms.CharField(max_length=30)
+    descricao = forms.CharField(max_length=60)
+
+    def __int__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['descricao'].widget = forms.Textarea()
--- livro/views.py
+++ livro/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from usuarios.models import Usuario
 from .models import Livros, Categoria, Emprestimos
-from .forms import Cadastro_livro
+from .forms import Cadastro_livro, Cadastro_categoria
+
 
 # Create your views here.
 
@@ -10,10 +11,20 @@ def home(request):
     if request.session.get('usuario'):
         usuario = Usuario.objects.get(id = request.session['usuario'])
         livros = Livros.objects.filter(usuario = usuario)
+        status_categoria = request.GET.get('cadastro_categoria')
+        status_livro = request.GET.get('cadastro_livro')
         form = Cadastro_livro()
+        form.fields['usuario'].initial = request.session['usuario']
+        form.fields['categoria'].queryset = Categoria.objects.filter(usuario = usuario)
+        form_categoria = Cadastro_categoria()
+
         return render(request, 'home.html', {'livros': livros,
                                              'usuario_logado': request.session.get('usuario'),
-                                             'form': form})
+                                             'form': form,
+                                             'id_livro': id,
+                                             'form_categoria': form_categoria,
+                                             'status_categoria': status_categoria,
+                                             'status_livro': status_livro})
     else:
         return redirect('/auth/login/?status=2')
 
@@ -26,7 +37,8 @@ def ver_livros(request, id):
             return render(request,'ver_livro.html', {'livro': livros,
                                                      'categoria_livro': categoria_livro,
                                                      'emprestimos':emprestimos,
-                                                     'usuario_logado': request.session.get('usuario')})
+                                                     'usuario_logado': request.session.get('usuario'),
+                                                     'id_livro': id})
         else:
             return HttpResponse('Esse livro não é seu')
     return redirect('auth/login/?status=2')
@@ -37,5 +49,24 @@ def cadastrar_livro(request):
 
         if form.is_valid():
             form.save()
+            return redirect('/livro/home/?cadastro_livro=1')
         else:
             return HttpResponse('Dados Invalidos')
+
+def excluir_livro(request, id):
+    livro = Livros.objects.get(id = id).delete()
+    return redirect('/livro/home')
+
+def cadastrar_categoria(request):
+    if request.method == 'POST':
+        form = Cadastro_categoria(request.POST)
+        nome = form.data['nome']
+        descricao = form.data['descricao']
+        id_usuario = request.POST.get('usuario')
+        if int(id_usuario) == int(request.session.get('usuario')):
+            user = Usuario.objects.get(id = id_usuario)
+            categoria = Categoria(nome=nome, descricao=descricao, usuario=user)
+            categoria.save()
+            return redirect('/livro/home/?cadastro_categoria=1')
+        else:
+            return HttpResponse('Erro')
